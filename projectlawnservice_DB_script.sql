fd1daeb1d657661cf76b257a42b3e86a957bd22a
KWoolman <48739472+KWoolman@users.noreply.github.com>
2019-04-22 21:22:17 -0400

    Update projectlawnservice_DB_script.sql

--- projectlawnservice_DB_script.sql
+++ projectlawnservice_DB_script.sql
@@ -5,20 +5,18 @@ CREATE TABLE customer (
   name VARCHAR(45) NOT NULL,
   email VARCHAR(45) NOT NULL,
   address VARCHAR(45) NOT NULL,
-  phone VARCHAR(45) NOT NULL,
-  city VARCHAR(45) NOT NULL,
-  zipcode VARCHAR(45) NOT NULL,
-  credit_number VARCHAR(45) NOT NULL,
-  expiration_number VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id)
-  );
+  phone VARCHAR(10) NOT NULL,
+  city VARCHAR(10) NOT NULL,
+  zipcode VARCHAR(5) NOT NULL,
+  credit_number VARCHAR(16) NOT NULL,
+  expiration_number VARCHAR(4) NOT NULL,
+  PRIMARY KEY (id));
 
 -- Create Table Category
 CREATE TABLE category (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id)
-  );
+  PRIMARY KEY (id));
 
 -- Create Table Product
 CREATE TABLE product (
@@ -32,8 +30,7 @@ CREATE TABLE product (
   CONSTRAINT fk_category
     FOREIGN KEY (category_id)
     REFERENCES category (id)
-    ON DELETE CASCADE
-    );
+    ON DELETE CASCADE);
 
 -- Create Table Customer_order
 CREATE TABLE customer_order (
@@ -43,24 +40,22 @@ CREATE TABLE customer_order (
   confirmation_number INT NOT NULL,
   customer_id INT UNSIGNED NOT NULL,
   PRIMARY KEY (id),  
-  CONSTRAINT fk_customer_order_customer1
+  CONSTRAINT fk_customer_order_customer
     FOREIGN KEY (customer_id)
     REFERENCES customer (id)
-    ON DELETE CASCADE
-    );
+    ON DELETE CASCADE);
 
--- Create Table Customer_order_has_product
-CREATE TABLE customer_order_has_product (
+-- Create Table Ordered_product
+CREATE TABLE ordered_product (
   customer_order_id INT UNSIGNED NOT NULL,
   product_id INT UNSIGNED NOT NULL,
   quantity SMALLINT UNSIGNED NOT NULL,
   PRIMARY KEY (customer_order_id, product_id),  
-  CONSTRAINT fk_customer_order_has_product_customer_order1
+  CONSTRAINT fk_ordered_customer_order
     FOREIGN KEY (customer_order_id)
     REFERENCES customer_order (id)
     ON DELETE CASCADE,
-  CONSTRAINT fk_customer_order_has_product_product1
+  CONSTRAINT fk_ordered_product_product
     FOREIGN KEY (product_id)
     REFERENCES product (id)
-    ON DELETE CASCADE
-    );
+    ON DELETE CASCADE);
