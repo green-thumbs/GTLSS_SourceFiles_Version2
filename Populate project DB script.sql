2d47cd1ed1170a05a6deb1ac0bacaf4abc2815a3
KWoolman <48739472+KWoolman@users.noreply.github.com>
2019-04-23 23:15:55 -0400

    Populate project DB

---
+++ Populate project DB script.sql
@@ -0,0 +1,16 @@
+-- Insert Products
+INSERT INTO product (name, price, description, category_id) VALUES ('Mowing Small Yard', 30.00, 'Lot Size 10ftx300ft', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Mowing Medium Yard', 40.00, 'Lot-Size 300ftX500ft', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Mowing Large Yard', 70.00, '500ftX700ft', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Tree Trimming Large', 100.00, '0ft-100ft tall, 0-10 trees', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Tree Trimming Medium', 60.00, '0ft-50ft, 0-10 tress', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Tree Trimming Small', 40.00, '0ft-30ft, 0-10 trees', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Shrub Trimming Large', 50.00, '0-50 shrubs', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Shrub Trimming Medium', 30.00, '0-30 shrubs', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Shrub Trimming Small', 20.00, '0-20 shrubs', 1);
+INSERT INTO product (name, price, description, category_id) VALUES ('Perennials', 12.95, 'Set of 6 flower plants: red, white, and yellow color', 2);
+INSERT INTO product (name, price, description, category_id) VALUES ('Annuals', 5.95, 'Set of 12 flower plants: red, white, and yellow color', 2);
+INSERT INTO product (name, price, description, category_id) VALUES ('Vegetables', 4.95, 'Set of 9 plants: Tomatoes, Bell peppers, and Chillies', 2);
+INSERT INTO product (name, price, description, category_id) VALUES ('Mulch', 5.95, 'Bag size 10lbs covers 100sqft', 2);
+INSERT INTO product (name, price, description, category_id) VALUES ('TopSoil', 10.00, '20lbs', 2);
+INSERT INTO product (name, price, description, category_id) VALUES ('Fertilizer', 21.95, '50lbs covers 1500sqft', 2);
